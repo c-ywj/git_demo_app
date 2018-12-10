7479fd6a0ebaa1f5eb0296538a551f0ccbb9364d
cyan <cyan24@my.bcit.ca>
2018-12-10 09:58:45 -0800

    bcit-internal-project-vs-git

---
+++ git_demo_app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace git_demo_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
